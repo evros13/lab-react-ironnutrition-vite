e7353fb95f1c2d8a421d69e06e4de5dda78be122
Edna Vidal <ednavidalros@gmail.com>
2023-08-23 16:49:32 +0200

    "Bonus 6"

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,12 @@
 import "./App.css";
-import foodsJson from "./foods.json";
-import { useState } from "react";
-import FoodBox from "./components/FoodBox";
-import AddFoodForm from "./components/AddFoodForm";
+import FoodList from "./components/FoodList"
 
 function App() {
-
-  const [foods, setFoods] = useState(foodsJson)
-
-
-  function removeFood(foodId) {
-
-    const removedFoodArray = foods.filter((foodToDelete) => {
-      return foodToDelete.id !== foodId
-    })
-
-    setFoods(removedFoodArray)
-  }
-
-  function addFood(foodToAdd) {
-    const addedFoodArray = [foodToAdd, ...foods]
-
-    setFoods(addedFoodArray)
-  }
-  
   return (
-
-    
-    <div className="container">
-
-    <AddFoodForm addFood={addFood}/>
-      {foods.map((food) => {
-          return (
-            <FoodBox key={food.id} food={food} removeFood={removeFood}/>
-          )
-        })}
+    <div className="App">
+      <FoodList />
     </div>
   );
 }
 
-export default App;
+export default App
